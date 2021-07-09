1e212d227546c7b3e193aac8a6415dbd14530719
Jason Wilson <84356946+jrwilson89@users.noreply.github.com>
2021-07-09 01:12:36 +0000

    Completed all code and passed all tests

--- message.js
+++ message.js
@@ -1,5 +1,43 @@
 class Message {
    // Write code here!
+   constructor(name, commands) {
+     this.name=name;
+     if (!name) {
+       throw Error("Message name required.");
+     }
+     this.commands=commands     
+   }
 }
 
+//let message = new Message("homer", "move")
+
 module.exports = Message;
+
+// class Command {
+//    constructor(commandType, value) {
+//      this.commandType = commandType;
+//      if (!commandType) {
+//        throw Error("Command type required.");
+//      }
+//      this.value = value;
+//    }
+ 
+//  }
+ 
+//  module.exports = Command;
+
+/*
+ it("throws error if a name is NOT passed into the constructor as the first parameter", function() {
+    expect( function() { new Message();}).toThrow(new Error('Message name required.'));
+  });
+
+  it("constructor sets name", function() {
+    let message = new Message('New message!');
+    expect(message.name).toEqual('New message!');
+  });
+
+  it("contains a commands array passed into the constructor as 2nd argument", function() {
+    let commands = [new Command('STATUS_CHECK'), new Command('MOVE', 20)];
+    let message = new Message('Another message!', commands);
+    expect(message.commands).toEqual(commands);
+*/
--- rover.js
+++ rover.js
@@ -1,5 +1,41 @@
 class Rover {
-   // Write code here!
+   // Write code here!+
+   constructor(position, mode='NORMAL', generatorWatts=110) {
+     this.position=position;
+     this.mode=mode;
+     this.generatorWatts=generatorWatts;
+   }   
+   receiveMessage(message) {
+          let roverMessage = {};
+          let results=[];
+     //roverMessage["message"]=message.name;
+       if (message.commands) {
+       for (let i=0; i<message.commands.length; i++) {
+         const command=message.commands[i];
+         if (command.commandType === 'STATUS_CHECK') {
+           results.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
+         }
+         if (command.commandType === 'MODE_CHANGE') {
+           this.mode=command.value
+           results.push({completed: true})
+         }
+         if (command.commandType === 'MOVE') {
+          //  this.mode=command.value
+           if (this.mode === 'LOW_POWER') {
+             results.push({completed: false, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}});
+           } else {
+             this.position=command.value
+             results.push({completed: true, roverStatus: {mode: this.mode, generatorWatts: this.generatorWatts, position: this.position}})            
+           }          
+         }              
+           
+       }
+     }
+     return {message: message.name, results};
+      // if (results==='MOVE')
+
+         
+   }
 }
 
 module.exports = Rover;
